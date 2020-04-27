565080a553895c8652ff95953d96f7d84ecafecb
Esteban Notebook <the4horsemen@gmail.com>
2020-04-27 18:28:47 -0300

    CAMBIOS

--- cronsqlmap.sh
+++ cronsqlmap.sh
@@ -5,7 +5,6 @@ source ~/.bash_aliases
 # a partir de un dominio (primera linea de urls.txt), obtiene waybackurls, ejecuta sqlmap para cada una
 # luego quita la primer linea de urls.txt y la agrega al final del archivo, para realizar un ciclo infinito sobre esos dominios
 primera=$(popandpull urls.txt)
-echo "$primera" | waybackurls | grep "\?" > tmp.txt
-uniqueurls tmp.txt batch$primera.txt && rm tmp.txt
+echo "$primera" | waybackurls | grep "\?" > batch$primera.txt
 batchsqlmap batch$primera.txt &
 wait
